f89e2f469e696bcd707255854c1f6a3a8d564547
unknown <morzmamun@gmail.com>
2024-05-07 02:08:51 +0600

    type assertion in typescript

---
+++ module2/type-assertion.ts
@@ -0,0 +1,41 @@
+{
+  //
+
+  // type assertion -> define any variable type with 'as' and make sure that is the type of the variable is called type assertion.
+
+  let anything: any;
+
+  anything = "Next level web development";
+
+  anything = 222;
+
+  //   (anything as string).
+
+  const kgToGm = (value: string | number) : string | number | undefined => {
+    if (typeof value === "string") {
+      const convertedValue = parseFloat(value) * 1000;
+      return convertedValue
+    }
+    if(typeof value === 'number'){
+        return value*1000
+    }
+  };
+
+  const result1 = kgToGm(1000) as number // it is type assertion
+  const result2 = kgToGm('1000') as string // it is type assertion
+
+  
+
+  type CustomMsg = {
+    message: string
+  }
+
+  try{
+
+  }catch(error){
+    console.log((error as CustomMsg).message);
+    
+  }
+
+  //
+}
