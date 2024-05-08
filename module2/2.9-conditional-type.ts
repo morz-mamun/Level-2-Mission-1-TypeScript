647030c8a751fd028a208b44bc61ba5bfb5a910c
unknown <morzmamun@gmail.com>
2024-05-09 00:48:22 +0600

    conditional type in typescript

---
+++ module2/2.9-conditional-type.ts
@@ -0,0 +1,22 @@
+{
+  // conditional type
+
+  type a1 = null;
+  type b1 = string;
+
+  type x = a1 extends number ? true : false; // conditional type
+  type y = a1 extends number ? true : b1 extends undefined ? true : any; // this is also a conditional type
+
+  //   example-> 
+
+  type Sheik = {
+    bike: string;
+    car: string;
+    ship: string;
+    plane: string
+  }
+
+  type CheckVehicle<T> = T extends keyof Sheik ? true : false
+  
+  type hasBike = CheckVehicle<'plane'>
+}
