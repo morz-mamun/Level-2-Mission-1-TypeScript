5af29e649ef16b0617bb6bf99adaaf1ebeaeef27
unknown <morzmamun@gmail.com>
2024-05-09 15:13:41 +0600

    mapped type and look up type

--- module2/2.10-mapped-type.ts
+++ module2/2.10-mapped-type.ts
@@ -12,33 +12,19 @@
 
   //typescript mapped type ->
 
-//   type AreaNumber = {
-//     height: number;
-//     width: number;
-//   };
-
-//   type Height = AreaNumber["height"]; // look up type
-
-//   type AreaString <T> = {
-//     [key in keyof T]: T[key]; // this is mapped type
-//   };
-
-//   const area1 : AreaString<{height: string; width: number}> = {
-//     height: '100',
-//     width: 50
-//   }
-
   type AreaString = {
     height: string;
-    width: string
-  }
+    width: string;
+  };
 
-  type AreaNumber <T> = {
-    [key in keyof T] : T[key] // look up type and mapped type
-  }
+  type Height = AreaString["height"]; // look up type
 
-  const area1 : AreaNumber<{height: string; width: number}> = {
-    height: '100',
-    width: 50
-  }
+  type AreaNumber<T> = {
+    [key in keyof T]: T[key]; // look up type and mapped type
+  };
+
+  const area1: AreaNumber<{ height: string; width: number }> = {
+    height: "100",
+    width: 50,
+  };
 }
